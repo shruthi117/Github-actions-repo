75e59b5145e98b8fc022ec735604834920a936a1
shruthi117 <124680890+shruthi117@users.noreply.github.com>
2025-02-12 15:32:38 +0530

    Update package.js

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 {
   "name": "nodejs-express-on-aws-ec2",
-  "version": "0.0.1",
   "scripts": {
     "start": "node app.js",
     "build": "npm run build"
   },
+  "version": "0.0.1",
   "dependencies": {
     "express": "4.17.1"
   }
 }
-
