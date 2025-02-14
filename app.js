abc66a433d089346ffdaa12f1d0dc2ba92c79c48
shruthi117 <124680890+shruthi117@users.noreply.github.com>
2025-02-14 14:30:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Node.js Application</h1> <h4>Message: Hello! from Node.js application Every</h4> <p>Version 1.1</p>');
+  res.send(`<h1>Welcome to Demo on GitHub Actions-Devops</h1>`);
 })
 
 app.get('/products', (req, res) => {
@@ -20,5 +20,5 @@ app.get('/products', (req, res) => {
 })
 
 app.listen(port, ()=> {
-  console.log(Node.js app is up and listening on the port: ${port});
+  console.log(`Demo app is up and listening to port: ${port}`);
 })
