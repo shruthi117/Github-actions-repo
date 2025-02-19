32b06429da6847d499d4bac14f9fd1897b1537c3
shruthi117 <124680890+shruthi117@users.noreply.github.com>
2025-02-19 17:53:56 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,12 +4,6 @@ set -e
 echo "🔐 Authenticating AWS ECR..."
 aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 879381243591.dkr.ecr.ap-south-1.amazonaws.com
 
-echo "🐳 Building Docker image..."
-docker build -t 879381243591.dkr.ecr.ap-south-1.amazonaws.com/demorepo:latest .
-
-echo "📤 Pushing Docker image to Amazon ECR..."
-docker push 879381243591.dkr.ecr.ap-south-1.amazonaws.com/demorepo:latest
-
 echo "⬇️ Pulling latest Docker image on EC2..."
 sudo docker pull 879381243591.dkr.ecr.ap-south-1.amazonaws.com/demorepo:latest
 
