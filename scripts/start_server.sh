ab787242349a0b0e0de3293e817c85d9978b6cd5
shruthi117 <124680890+shruthi117@users.noreply.github.com>
2025-02-19 18:07:52 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "🛑 Stopping old application (if running)..."
-sudo docker stop myappcontainer || true
-sudo docker rm myappcontainer || true
+sudo docker stop demorepo-container || true
+sudo docker rm demorepo-container || true
 
 echo "🚀 Starting new container..."
 sudo docker run -d --restart always -p 80:3000 --name myapp-container 879381243591.dkr.ecr.ap-south-1.amazonaws.com/demorepo:latest
