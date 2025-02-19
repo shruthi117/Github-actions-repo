eee7ff09dbd941d5b9f2ce7ad04e23c2c7198812
shruthi117 <124680890+shruthi117@users.noreply.github.com>
2025-02-19 18:03:51 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "🛑 Stopping old application (if running)..."
+sudo docker stop myappcontainer || true
+sudo docker rm myappcontainer || true
+
 echo "🚀 Starting new container..."
 sudo docker run -d --restart always -p 80:3000 --name myapp-container 879381243591.dkr.ecr.ap-south-1.amazonaws.com/demorepo:latest
